388ff23e061ea5f32cb74a50abd9931bf6a3cde4
juniorsohn <juniorsohn@hotmail.com>
2024-03-19 13:43:14 -0300

    removido print l26

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ cont_Cli = 0
 cont_Tran = 0
 
 async def setup_identity(identity, trustee):
-    print('cheguei no setup identity')
     did_safe = 'V4SGRU86Z58d6TV7PBUe6f'
     verkey_safe = '~CoRER63DVYnWZtK8uAzNbx'
     (identity['did'], identity['key']) = await did.create_and_store_my_did(identity['wallet'], "{}")
