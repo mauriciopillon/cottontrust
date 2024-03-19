1e332193376020b31376cb98138d5659aa4cce17
tompagli <pagliuso.tom@gmail.com>
2024-03-19 14:13:48 -0300

    small fixes and tweaks

--- main.py
+++ main.py
@@ -8,16 +8,16 @@ from indy import pool, wallet, did, ledger
 from indy.error import ErrorCode, IndyError
 
 UBAs = []
-Fardinhos = []
-Clientes = []
+Bale = []
+Clients = []
 
 
-tempos_transacao = []
-tempo_criacao = []
+time_transaction = []
+time_create = []
 
 
 cont_Uba = 0
-cont_Far = 0
+cont_Bale = 0
 cont_Cli = 0
 
 cont_Tran = 0
@@ -44,105 +44,105 @@ async def create_wallet(Entity):
     Entity['wallet'] = await wallet.open_wallet(Entity['wallet_config'],
                                                   Entity['wallet_credentials'])
     
-def create_seed(contador, nome):
-        seed =  str(nome) + str(contador) + 'A0000000000000000000000000000000000' 
+def create_seed(counter, name):
+        seed =  str(name) + str(counter) + 'A0000000000000000000000000000000000' 
         return seed[:32]
 
-async def create_cliente(pool_, cliente_data, trustee):
+async def create_client(pool_, client_data, trustee):
     global cont_Cli
     cont_Cli += 1
     
 
-    print(f"\nCriando Clientes {cont_Cli} - Cadastre")
+    print(f"\nCreating Clients {cont_Cli} - Sign up")
 
 
-    CLIENTE = {
-        'name': cliente_data['name'],
-        'Endereco - Rua': cliente_data['Endereco - Rua'],
-        'Endereco - Bairro': cliente_data['Endereco - Bairro'],
-        'Endereco - Cidade': cliente_data['Endereco - Cidade'],
-        'Endereco - Estado': cliente_data['Endereco - Estado'],
-        'Endereco - Pais': cliente_data['Endereco - Pais'],
-        'wallet_config': json.dumps({'id': cliente_data['wallet_config']}),
-        'wallet_credentials': json.dumps({'key': cliente_data['wallet_credentials']}),
+    CLIENT = {
+        'name': client_data['name'],
+        'Address - Street': client_data['Address - Street'],
+        'Address - Neighborhood': client_data['Address - Neighborhood'],
+        'Address - City': client_data['Address - City'],
+        'Address - State': client_data['Address - State'],
+        'Address - Country': client_data['Address - Country'],
+        'wallet_config': json.dumps({'id': client_data['wallet_config']}),
+        'wallet_credentials': json.dumps({'key': client_data['wallet_credentials']}),
         'pool': pool_['handle'],
 
-        'seed': create_seed(cont_Cli, cliente_data['name']),
-        "balance": cliente_data['balance'],
-        "quero_fardinho": cliente_data['quero_fardinho'],
-        "quant_fardinho": cliente_data['quant_fardinho']
+        'seed': create_seed(cont_Cli, client_data['name']),
+        "balance": client_data['balance'],
+        "req_bale": client_data['req_bale'],
+        "quant_bale": client_data['quant_bale']
 
     }
 
-    await create_wallet(CLIENTE)
-    CLIENTE["did_info"] = json.dumps({'seed': CLIENTE['seed']})
+    await create_wallet(CLIENT)
+    CLIENT["did_info"] = json.dumps({'seed': CLIENT['seed']})
 
-    CLIENTE['did'], CLIENTE['key'] = await did.create_and_store_my_did(CLIENTE['wallet'], CLIENTE['did_info']) 
-    # AQUI EH A FUNCAO DE SUBMETER PARA O LEDGER
-    await setup_identity(CLIENTE, trustee)
-    Clientes.append(CLIENTE)  
+    CLIENT['did'], CLIENT['key'] = await did.create_and_store_my_did(CLIENT['wallet'], CLIENT['did_info']) 
+
+    await setup_identity(CLIENT, trustee)
+    Clients.append(CLIENT)  
     
 
-async def create_fardinho(pool_, fardinho_data):
-    global cont_Far
-    cont_Far += 1
+async def create_bale(pool_, bale_data):
+    global cont_Bale
+    cont_Bale += 1
     
 
-    print(f"Criando Fardinho {cont_Far} - Cadastre")
+    print(f"Creating bale {cont_Bale} - Sign up")
 
 
-    FARDINHO = {
-        'name': fardinho_data['name'],
-        'Identificador do Fardinho': fardinho_data['Identificador do Fardinho'],
-        'Identificador da Fazenda': fardinho_data['Identificador da Fazenda'],
-        'Identificador da UBA': fardinho_data['Identificador da UBA'],
-        'Safra': fardinho_data['Safra'],
-        'Talhao': fardinho_data['Talhao'],
-        'Data da Colheita': fardinho_data['Data da Colheita'],
-        'Produto da Semente': fardinho_data['Produto da Semente'],
-        'Lote da Semente': fardinho_data['Lote da Semente'],
-        'Peso': fardinho_data['Peso'],
-        'wallet_config': json.dumps({'id': fardinho_data['wallet_config']}),
-        'wallet_credentials': json.dumps({'key': fardinho_data['wallet_credentials']}),
+    BALE = {
+        'name': bale_data['name'],
+        'Bale Identifier': bale_data['Bale Identifier'],
+        'Farm Identifier': bale_data['Farm Identifier'],
+        'UBA Identifier': bale_data['UBA Identifier'],
+        'Harvest Season': bale_data['Harvest Season'],
+        'Plot': bale_data['Plot'],
+        'Harvest Date': bale_data['Harvest Date'],
+        'Seed Product': bale_data['Seed Product'],
+        'Seed Lot': bale_data['Seed Lot'],
+        'Weight': bale_data['Weight'],
+        'wallet_config': json.dumps({'id': bale_data['wallet_config']}),
+        'wallet_credentials': json.dumps({'key': bale_data['wallet_credentials']}),
         'pool': pool_['handle'],
 
-        'seed': create_seed(cont_Far, fardinho_data['name']),
+        'seed': create_seed(cont_Bale, bale_data['name']),
         "balance": 1000
 
     }
 
-    await create_wallet(FARDINHO)
-    FARDINHO["did_info"] = json.dumps({'seed': FARDINHO['seed']})
-    FARDINHO['did'], FARDINHO['key'] = await did.create_and_store_my_did(FARDINHO['wallet'], FARDINHO['did_info'])
+    await create_wallet(BALE)
+    BALE["did_info"] = json.dumps({'seed': BALE['seed']})
+    BALE['did'], BALE['key'] = await did.create_and_store_my_did(BALE['wallet'], BALE['did_info'])
 
-    Fardinhos.append(FARDINHO) 
+    Bale.append(BALE) 
 
 async def create_uba(pool_, uba_data, trustee):
     global cont_Uba
     cont_Uba += 1
     
 
-    print(f"\nCriando UBA {cont_Uba} - Cadastre")
+    print(f"\Creating UBA {cont_Uba} - Sign Up")
     
 
 
     UBA = {
         'name': uba_data['name'],
-        'Codigo de Registro da UBA': uba_data['Codigo de Registro da UBA'],
+        'UBA registry code': uba_data['UBA registry code'],
         'CNPJ': uba_data['CNPJ'],
-        'Endereco - Rua': uba_data['Endereco - Rua'],
-        'Endereco - Bairro': uba_data['Endereco - Bairro'],
-        'Endereco - Cidade': uba_data['Endereco - Cidade'],
-        'Endereco - Estado': uba_data['Endereco - Estado'],
-        'Endereco - Pais': uba_data['Endereco - Pais'],
+        'Address - Street': uba_data['Address - Street'],
+        'Address - Neighborhood': uba_data['Address - Neighborhood'],
+        'Address - City': uba_data['Address - City'],
+        'Address - State': uba_data['Address - State'],
+        'Address - Country': uba_data['Address - Country'],
         'wallet_config': json.dumps({'id': uba_data['wallet_config']}),
         'wallet_credentials': json.dumps({'key': uba_data['wallet_credentials']}),
         'pool': pool_['handle'],
 
         'seed': create_seed(cont_Uba, uba_data['name']),
         "balance": uba_data['balance'],
-        "preco_fardinho": uba_data['preco_fardinho'],#add
-        "quant_fardinho": uba_data['quant_fardinho'] #add
+        "bale_price": uba_data['bale_price'],#add
+        "quant_bale": uba_data['quant_bale'] #add
     }
 
     
@@ -151,63 +151,54 @@ async def create_uba(pool_, uba_data, trustee):
     UBA["did_info"] = json.dumps({'seed': UBA['seed']})
     UBA['did'], UBA['key'] = await did.create_and_store_my_did(UBA['wallet'], UBA['did_info'])
 
-    # AQUI EH A FUNCAO DE SUBMETER PARA O LEDGER
     await setup_identity(UBA, trustee)
     UBAs.append(UBA)
     
-async def create_transaction(sender, receiver, custo_far, quant_far):
+async def create_transaction(sender, receiver, bale_cost, quant_bale):
     global cont_Tran
     cont_Tran += 1
 
-    amount = custo_far * quant_far
+    amount = bale_cost * quant_bale
     print("--------------------------------------------")
-    print(f"Iniciando trasacao {cont_Tran}:")
-    print(f"Saldo atual de {sender['name']}: R${sender['balance']},00")
-    print(f"Saldo atual de {receiver['name']}: R${receiver['balance']},00")
-    print(f"Quantidade de Fardinhos disponiveis em {receiver['name']}: {receiver['quant_fardinho']}")
-    print(f"Quantidade de Fardinhos que {sender['name']} quer comprar: {quant_far}")
-    print(f"Preco de cada Fardinho: R${custo_far},00")
-    print(f"Valor total da transacao: R${amount},00")
+    print(f"Starting transaction {cont_Tran}:")
+    print(f"Current balance of {sender['name']}: R${sender['balance']},00")
+    print(f"Current balance of {receiver['name']}: R${receiver['balance']},00")
+    print(f"Quantity of bales available in {receiver['name']}: {receiver['quant_bale']}")
+    print(f"Quantity of bales {sender['name']} wants to buy: {quant_bale}")
+    print(f"Price of each bale: R${bale_cost},00")
+    print(f"Total transaction value: R${amount},00")
+
 
     start_time = time.time()
 
-    # Verifique se o remetente tem saldo suficiente
     if sender['balance'] < amount:
-        print(f"{sender['name']} nao tem saldo suficiente para a transacao de compra dos fardinhos")
+        print(f"{sender['name']} has insufficient funds")
         return
 
-    # Atualize o saldo do remetente
     sender['balance'] -= amount
 
-    # Atualize a quantidade de Fardinhos do remetente
-    sender['quant_fardinho'] += quant_far
+    sender['quant_bale'] += quant_bale
 
-    # Construa a solicitação de atributo para o remetente
-    sender_attr_req = await ledger.build_attrib_request(sender['did'], sender['did'], None, json.dumps({'balance': sender['balance'], 'quant_fardinho': sender['quant_fardinho']}), None)
+    sender_attr_req = await ledger.build_attrib_request(sender['did'], sender['did'], None, json.dumps({'balance': sender['balance'], 'quant_bale': sender['quant_bale']}), None)
 
-    # Assine e envie a solicitação de atributo para o remetente
     await ledger.sign_and_submit_request(sender['pool'], sender['wallet'], sender['did'], sender_attr_req)
 
-    # Atualize o saldo do destinatário
     receiver['balance'] += amount
 
-    # Atualize a quantidade de Fardinhos do destinatário
-    receiver['quant_fardinho'] -= quant_far
+    receiver['quant_bale'] -= quant_bale
 
-    # Construa a solicitação de atributo para o destinatário
-    receiver_attr_req = await ledger.build_attrib_request(receiver['did'], receiver['did'], None, json.dumps({'balance': receiver['balance'], 'quant_fardinho': receiver['quant_fardinho']}), None)
+    receiver_attr_req = await ledger.build_attrib_request(receiver['did'], receiver['did'], None, json.dumps({'balance': receiver['balance'], 'quant_bale': receiver['quant_bale']}), None)
 
-    # Assine e envie a solicitação de atributo para o destinatário
     await ledger.sign_and_submit_request(receiver['pool'], receiver['wallet'], receiver['did'], receiver_attr_req)
 
     end_time = time.time()
     duration = end_time - start_time
-    print(f"\nA transacao levou {duration} segundos para ser concluida\n")
-    tempos_transacao.append(duration)
+    print(f"\nThe transaction took {duration} secs\n")
+    time_transaction.append(duration)
 
-    print(f"Transacao concluida: {sender['name']} enviou R${amount},00 para {receiver['name']} e recebeu {quant_far} Fardinhos")
-    print(f"Saldo atual de {sender['name']}: R${sender['balance']},00 e {sender['quant_fardinho']} Fardinhos")
-    print(f"Saldo atual de {receiver['name']}: R${receiver['balance']},00 e {receiver['quant_fardinho']} Fardinhos")
+    print(f"Sucessful Transaction: {sender['name']} sent R${amount},00 to {receiver['name']} and received {quant_bale} bales")
+    print(f"Current balance {sender['name']}: R${sender['balance']},00 and {sender['quant_bale']} bales")
+    print(f"Current balance {receiver['name']}: R${receiver['balance']},00 and {receiver['quant_bale']} bales")
     print("--------------------------------------------")
 
 async def run():
@@ -256,7 +247,7 @@ async def run():
         try:
             ubas_data = json.load(file)
         except json.JSONDecodeError:
-            print("Arquivo UBA esta vazio.\n")
+            print("UBA file empty.\n")
             ubas_data = []
 
     if ubas_data:
@@ -264,76 +255,76 @@ async def run():
             time_uba = time.time()
             await create_uba(pool_, uba_data, trustee)
             endtime_uba = time.time()
-            tempo_criacao.append(endtime_uba - time_uba)
+            time_create.append(endtime_uba - time_uba)
 
-        print("UBAs criados:\n")
+        print("UBAs created:\n")
         for item in UBAs:
             print(f"{item}\n")
 
-    # FARDINHOS -----------------------------------------------------------------------------------
+    # BALES -----------------------------------------------------------------------------------
 
     with open('models/fardinhos.json', 'r') as file:
         try:
-            fardinhos_data = json.load(file)
+            bale_data = json.load(file)
         except json.JSONDecodeError:
-            print("Arquivo FARDINHOS esta vazio.\n")
-            fardinhos_data = []
+            print("Bale file is empty.\n")
+            bale_data = []
 
-    if fardinhos_data:
-        for fardinho_data in fardinhos_data:
-            await create_fardinho(pool_, fardinho_data)
+    if bale_data:
+        for bale_data in bale_data:
+            await create_bale(pool_, bale_data)
 
-        print("Fardinhos criados:\n")
-        for item in Fardinhos:
+        print("Created bales:\n")
+        for item in Bale:
             print(f"{item}\n")
 
-    # CLIENTES -----------------------------------------------------------------------------------
+    # CLIENTS -----------------------------------------------------------------------------------
 
     with open('models/clientes.json', 'r') as file:
         try:
-            clientes_data = json.load(file)
+            clients_data = json.load(file)
         except json.JSONDecodeError:
-            print("Arquivo CLIENTES esta vazio.\n")
-            clientes_data = []
+            print("Client file is empty.\n")
+            clients_data = []
 
-    if clientes_data:
-        for cliente_data in clientes_data:
+    if clients_data:
+        for client_data in clients_data:
             time_cli = time.time()
-            await create_cliente(pool_, cliente_data, trustee)
+            await create_client(pool_, client_data, trustee)
             endtime_cli = time.time()
-            tempo_criacao.append(endtime_cli - time_cli)
+            time_create.append(endtime_cli - time_cli)
 
-        print("Clientes do mercado externo criados:\n")
-        for item in Clientes:
+        print("Clients created:\n")
+        for item in Clients:
             print(f"{item}\n")
 
-    # TRANSAÇÃO -----------------------------------------------------------------------------------------
+    # TRANSACTION -----------------------------------------------------------------------------------------
 
-    if UBAs and Clientes:
-        num_transacoes = 4 # Quantidade de transações
+    if UBAs and Clients:
+        num_trans = 4 # Quantidade de transações
 
-        for _ in range(num_transacoes):
+        for _ in range(num_trans):
 
-            sender= random.choice(Clientes)
+            sender= random.choice(Clients)
             receiver = random.choice(UBAs)
-            custo_far = receiver['preco_fardinho']
-            quant_far = sender['quero_fardinho']
+            bale_cost = receiver['bale_price']
+            quant_bale = sender['req_bale']
             
-            await create_transaction(sender, receiver, custo_far, quant_far)
+            await create_transaction(sender, receiver, bale_cost, quant_bale)
 
-    # TEMPOS --------------------------------------------------------------------------------------------
-    print("Escrevendo no arquivo CSV...")
-    print(f"Tempos de transacao: {tempos_transacao}")
-    print(f"Tempos de criacao: {tempo_criacao}")
+    # TIMES --------------------------------------------------------------------------------------------
+    print("Writing on CSV file...")
+    print(f"Transaction time: {time_transaction}")
+    print(f"Creation time: {time_create}")
 
-    with open('tempos.csv', 'a', newline='') as file:  # Abrir arquivo no modo de anexação
+    with open('time_data.csv', 'a', newline='') as file: 
         writer = csv.writer(file)
-        if not os.path.exists('tempos.csv') or os.stat('tempos.csv').st_size == 0:  # Se o arquivo não existir ou estiver vazio
-            writer.writerow(["Quant. De Entitys:", "Tempo de Transacao:", "Tempo de Criacao:"])  # Escrever cabeçalho
-        for t, tc in zip(tempos_transacao, tempo_criacao):
-            writer.writerow(["100", t, tc])  # Escrever dados
+        if not os.path.exists('time_data.csv') or os.stat('time_data.csv').st_size == 0:  
+            writer.writerow(["Quant. of Entitys:", "Transaction time:", "Creation time:"])  
+        for t, tc in zip(time_transaction, time_create):
+            writer.writerow(["100", t, tc])  
         writer.writerow([])
-print("Concluído.")
+print("Done.")
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run())
